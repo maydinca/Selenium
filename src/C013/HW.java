ad9a98f5da26c53a6777cf7e0cfa785c2595bebe
maydinca <maydinca@gmail.com>
2022-12-10 11:00:40 -0500

    Decemver 10 && Selenium HW

--- src/C013/HW.java
+++ src/C013/HW.java
@@ -1,7 +1,19 @@
 package C013;
 
 /**
- * https://the-internet.herokuapp.com/ siteye git checkbox a tikla tiklanabiliyorsa tikla tiklandiysa digerine gec..
+ * 1. Go to https://the-internet.herokuapp.com/
+ * 2. Find the Checkboxes link, click on it
+ * 3. Once you are on the Checkboxes homepage you will see two Checkboxes:
+ *       - checkbox 1
+ *       - checkbox 2
+ *
+ *   4. Using your Selenium knowledge. Please check and verify if each checkbox is:
+ *         1. isDisplayed (True or False) ?
+ *         2. isEnabled (T or F) ?
+ *         3. isSelected (T or F) ?
+ *
+ * 5. Print the results to the console.
+ * 6. Exit the browser.
  *
  */
 public class HW {
