1d42c2f79c79cf13515de588f9d79f46926a7fe4
maydinca <maydinca@gmail.com>
2022-12-10 11:00:40 -0500

    Decemver 10 && Selenium HW

---
+++ src/C013/HW2.java
@@ -0,0 +1,13 @@
+package C013;
+
+/**
+ * TC 4.1: Selecting Colors:
+ * Open the Chrome browser
+ * Go to provided “URL” page where checkboxes are present
+ * Loop through the list of elements and find Yellow & Orange colors
+ * Select both and print their names to the console
+ * Close the browser
+ * https://selenium08.blogspot.com/2019/07/check-box-and-radio-buttons.html
+ */
+public class HW2 {
+}
