6760e6d053bcd35273cdb6f5a76cf0bcdd15eece
maydinca <maydinca@gmail.com>
2022-12-12 23:27:44 -0500

    Java Recap Added

---
+++ src/RecapJava/switchCase.java
@@ -0,0 +1,15 @@
+package RecapJava;
+
+public class switchCase {
+    public static void main(String[] args) {
+
+        int day = 2;
+        String dayType  = switch(day) {
+            case 1, 2, 3, 4, 5 -> "Working day";
+            case 6, 7 -> "Weekend";
+            default -> "Invalid day";
+        };
+        System.out.println(dayType);
+    }
+}
+
