409e2f86e2ba05fcf0039dd0792becff91085b61
maydinca <maydinca@gmail.com>
2022-12-21 15:17:46 -0500

    C014 updated

--- src/C014/_01_AlertDemo.java
+++ src/C014/_01_AlertDemo.java
@@ -16,8 +16,8 @@ public class _01_AlertDemo {
         Thread.sleep(2000);
 
         // Question: What happens when we try to click on another element without handling an ALERT first?
-        // driver.findElement(By.xpath("//div[text()='Search']")).click();
-        // Answer: You will get "UnhandledAlertException" if you don't handle alert before doing anything else.
+                // driver.findElement(By.xpath("//div[text()='Search']")).click();
+                // Answer: You will get "UnhandledAlertException" if you don't handle alert before doing anything else.
 
         Alert alert = driver.switchTo().alert();             // We switch the focus of the web browser to the alert window
         String alertText = alert.getText();                  // I am storing the text from the Alert in a String var.
@@ -27,7 +27,7 @@ public class _01_AlertDemo {
 
         // Handling Confirmation Alert
         driver.findElement(By.cssSelector("button#confirm")).click();
-        Alert confirmationAlert= driver.switchTo().alert();
+        Alert confirmationAlert= driver.switchTo().alert();         // Note: For all alert types we use the same .switchTo().alert();
         String confirmationAlertText = confirmationAlert.getText();
         System.out.println("confirmationAlertText: " + confirmationAlertText);
         Thread.sleep(2000);
@@ -39,25 +39,26 @@ public class _01_AlertDemo {
         driver.findElement(By.id("prompt")).click();               //identify webElement for the Alert and click on it
         Alert promptAlert =driver.switchTo().alert();              //Switching to Alert (from the browser) to handle it
         String promptAlertText=promptAlert.getText();              //Get the content /text of the Alert
-        System.out.println("promptAlertText = " + promptAlertText);
+        System.out.println("promptAlertText : " + promptAlertText);
         Thread.sleep(2000);                                  // Add time only when needed. This is totally optional
         promptAlert.sendKeys("Mehmet");                  // I am sending text/value into the input text box
         promptAlert.accept();                                      //Press ok button to close the alert
 
 
         //Lets verify our entered text
+
+       // System.out.println(driver.findElement(By.xpath("//div[contains(text(),'Mehmet')]")).getText(); //in one line
         String enteredText = driver.findElement(By.xpath("//div[contains(text(),'Mehmet')]")).getText();
-        System.out.println("enteredText"+enteredText);
+        System.out.println("enteredText : "+enteredText);
 
+        String expectedText ="Mehmet";  //Negative Testing is "Happy Path Testing"
 
-        String expectedText ="Mehmet";
         if(enteredText.contains(expectedText)) {
             System.out.println("Text is added successfully.Test Passed.");
 
         } else {
             System.err.println("Text is not added successfully.Test failed.");
         }
-
         tearDown();
     }
 }
