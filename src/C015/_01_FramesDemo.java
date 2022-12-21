a567def7584eea906ab2511542db3c89c1d8fd9a
maydinca <maydinca@gmail.com>
2022-12-20 21:01:13 -0500

    IFrame

---
+++ src/C015/_01_FramesDemo.java
@@ -0,0 +1,47 @@
+package C015;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static C011_utils.BaseClass.*;
+
+public class _01_FramesDemo {
+    public static void main(String[] args) throws InterruptedException {
+        setUp(); //http://www.uitestpractice.com/Students/Switchto
+
+        //Trying to send Text 'SDET' to the Frame 's input text-box
+        //Note: Before interacting with a frame , first we need to switch to it, then take action. Otherwise, we get Exception.
+
+
+//        driver.findElement(By.cssSelector("#name")).sendKeys("SDET");
+
+        /**
+         * We can switch to frame using three different methods,
+         * 1.By index
+         * 2.By ID or Name
+         * 3.By WebElement of the frame/iframe         *
+         *
+         */
+
+        // 1., By Index
+        driver.switchTo ().frame(0);
+        WebElement textBox= driver.findElement(By.id("name"));
+        textBox.sendKeys("SDET");
+
+        driver.switchTo().defaultContent();
+
+        //Let's try to get text from the main window/browser without switching first
+        System.out.println(driver.findElement(By.xpath("//*[contains(text(),'2022')]")).getText());
+
+        Thread.sleep(2000);
+        // 2. By Name or ID
+        driver.switchTo().frame("iframe_a");
+        textBox.clear();
+        textBox.sendKeys("QA Engineer");
+
+        tearDown();
+
+
+
+    }
+}
