b189c82d489158c543d6f6cf618edaa25d690c12
“maydinca” <maydinca@gmail.com>
2023-01-08 15:32:03 -0500

    C017 updated

--- src/C017/_02_ImplicitWait_Task.java
+++ src/C017/_02_ImplicitWait_Task.java
@@ -10,7 +10,7 @@ import static C011_utils.BaseClass.*;
 /**
  *  Task: Get the text from a link which will load a paragraph when you click on it.
  *    1. Go to this URL: http://uitestpractice.com/Students/Contact
- *    2. Click on the link where it says 'This is a Ajax link'
+ *    2. Click on the link where it says 'This is an Ajax link'
  *    3. First use without wait, handle the exception, should say "Element is not found, Please wait longer for element to become visible".
  *    4. Wait for its content to load and retrieve the text from it and print it to the console.
  *
@@ -18,9 +18,10 @@ import static C011_utils.BaseClass.*;
 public class _02_ImplicitWait_Task {
     public static void main(String[] args) {
         setUp("http://uitestpractice.com/Students/Contact");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));       // This is a GLOBAL wait. Applies to entire elements in the class.
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15)); // This is a GLOBAL wait. Applies to entire elements in the class.
 
         try {
+            driver.manage().window().maximize();
             driver.findElement(By.linkText("This is a Ajax link")).click();
             System.out.println(driver.findElement(By.className("ContactUs")).getText());
         } catch (NoSuchElementException e) {
