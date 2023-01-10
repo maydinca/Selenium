6ee0398b0793ada4989fd440cf740d142f0868e9
“maydinca” <maydinca@gmail.com>
2023-01-09 22:38:36 -0500

    C017,C018 and config file are  updated

--- src/C011_utils/BaseClass.java
+++ src/C011_utils/BaseClass.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.time.Duration;
+
 // NOTE: THIS CLASS IS USED TO LAUNCH AND QUIT THE BROWSER.
 public class BaseClass {
     public static WebDriver driver;
@@ -30,6 +32,8 @@ public class BaseClass {
 
 //            driver.get(ConfigsReader.getProperties("url"));
                driver.get(url);               //URL i config icerisinden almamak icin ..
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
     }
 
--- src/C017/_07_PageLoadTimeOut.java
+++ src/C017/_07_PageLoadTimeOut.java
@@ -1,5 +1,6 @@
 package C017;
 
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,32 +9,33 @@ import java.time.Instant;
 
 public class _07_PageLoadTimeOut {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");  // chromedriver.exe
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");  // WINDOWS USERS ONLY: chromedriver.exe
         WebDriver driver = new ChromeDriver();
+
         /**
-         * I am telling the browser, stop Looking after given time, in this case,stop loading after 3 seconds.
-         * In other words, I am telling the webpage = "You got 1 sec to load OR I will throw an exception"
+         *  I am telling the browser, stop looking after given time, in this case, stop loading after 3 seconds.
+         *  In other words, I am telling the webpage = "you got 1 sec to load OR I will throw an exception"
          */
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(3)); //<== This is Page Load TimeOut Wait.
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(3));   // <== This is PageLoadTimeOut Wait.
 
         Instant startTime = Instant.now();
-        System.out.println("startTime = " + startTime);
+        System.out.println("startTime: " + startTime);
 
-        try{
-        driver.get("https://amazon.com/");
-
-        }catch (Exception e){
+        try {
+            driver.get("https://exelenter.com");
+        } catch (TimeoutException e) {
             e.printStackTrace();
-            System.out.println("========> Too Slow to load.... Add More Time <======= ");
+            System.out.println("===> TOO SLOW TO LOAD... ADD MORE TIME <====");
         }
 
-
         Instant endTime = Instant.now();
-        System.out.println("endTime = " + endTime);
+        System.out.println("endTime: " + endTime);
 
-        Duration pageLoadTime=Duration.between(startTime,endTime);
-        System.out.println("pageLoadTime = " + pageLoadTime.toMillis());
+        Duration pageLoadTime = Duration.between(startTime, endTime);
+        System.out.println("pageLoadTime: = " + pageLoadTime.toMillis());
+
         System.out.println(driver.getTitle());
+
         driver.close();
 
     }
