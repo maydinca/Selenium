8ecb28e245df15fba50b0d22921100b1d75dd825
“maydinca” <maydinca@gmail.com>
2023-01-10 21:46:21 -0500

    _02_RadioOrCheckBoxTest added

---
+++ src/C018_DevelopFunctions/_02_RadioOrCheckBoxTest.java
@@ -0,0 +1,46 @@
+package C018_DevelopFunctions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static C011_utils.BaseClass.*;
+
+public class _02_RadioOrCheckBoxTest {
+    public static void main(String[] args) {
+        setUp("https://selenium08.blogspot.com/2019/07/check-box-and-radio-buttons.html");
+
+        //Loop through all colors and select and click on Green color
+        List<WebElement> colorList= driver.findElements(By.name("color"));
+        List<WebElement> browserList= driver.findElements(By.name("browser"));
+
+        String expectedValue="green";
+        clickRadioORCheckbox(colorList,expectedValue);
+        clickRadioORCheckbox(browserList,"Mozilla");
+
+//        for (WebElement color :colorList) {
+//            String colorAttribute =color.getAttribute("value");
+//            if (colorAttribute.equalsIgnoreCase(expectedValue)){
+//                color.click();
+//                break;
+//            }
+//        }
+
+    tearDown();
+
+
+    }
+
+
+    public static void clickRadioORCheckbox(List<WebElement>radioOrCheckbox,String expectedValue){
+
+        for (WebElement element :radioOrCheckbox) {
+            String actualValue = element.getAttribute("value");
+            if (actualValue.equals(expectedValue)){
+                element.click();
+                break;
+            }
+        }
+    }
+}
